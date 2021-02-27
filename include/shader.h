ce3c8917a333b73904e3b4763e5eb66ba2c87791
hungr <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2021-02-27 12:24:07 +0900

    add shader

---
+++ include/shader.h
@@ -0,0 +1,31 @@
+//
+// Created by hungr on 2021/02/27.
+//
+
+#ifndef GL_SHADER_H
+#define GL_SHADER_H
+
+#include <string>
+
+#include <GL/glew.h>
+
+namespace gl {
+    class shader_program {
+    public:
+        shader_program();
+        shader_program(const shader_program& obj) = delete;
+        shader_program(shader_program&& obj) noexcept ;
+        ~shader_program();
+        [[nodiscard]] bool enabled() const noexcept;
+        [[nodiscard]] GLuint handle() const noexcept;
+        void reset();
+        bool add_shader(const std::string &file, GLenum type);
+        bool link();
+        void use() const;
+        void unuse() const;
+    private:
+        GLuint m_handle;
+    };
+}
+
+#endif //GL_SHADER_H
