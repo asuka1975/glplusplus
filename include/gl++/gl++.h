d5f946a3fdc7943abb3797c8e177a7050b166b10
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2021-08-27 10:33:33 +0900

    feat: add header file whole including

---
+++ include/gl++/gl++.h
@@ -0,0 +1,20 @@
+//
+// Created by shikina on 2021/08/27.
+//
+
+#ifndef GL_GL_H
+#define GL_GL_H
+
+#ifndef GLPLUSPLUS_NO_SHADER
+#include <gl++/shader.h>
+#endif
+
+#ifndef GLPLUSPLUS_NO_VERTEX_BUFFER
+#include <gl++/vertex_buffer.h.h>
+#endif
+
+#ifndef GLPLUSPLUS_NO_VERTEX_ARRAY
+#include <gl++/vertex_array.h.h>
+#endif
+
+#endif //GL_GL_H
